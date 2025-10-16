e56ff4b0e32d269329a34bb98fa0dd914526d096
nxyy <nxyy@nxyy.store>
2025-10-16 13:14:33 -0400

    authorization

--- src/api/playing/[id].ts
+++ src/api/playing/[id].ts
@@ -5,7 +5,7 @@ export default async (req: RoboRequest) => {
   const response = await fetch(`https://api.warm.lat/playing/${userId}`, {
     method: 'GET',
     headers: {
-      'Authorization': req.headers.get('authorization') || '',
+      'Authorization': req.headers.get('authorization') || 'canister-dipped-expletive-dab-slug-lustiness',
       'Content-Type': 'application/json'
     }
   })
