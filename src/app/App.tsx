b4348cd605ff2944cf7916ab672963ef9453e94c
nxyy <nxyy@nxyy.store>
2025-10-16 12:27:20 -0400

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -14,7 +14,7 @@ import './App.css'
  */
 export default function App() {
 	return (
-		<DiscordContextProvider authenticate=true scope={['identify', 'guilds', 'rpc.activities.write']}>
+		<DiscordContextProvider authenticate={true} scope={['identify', 'guilds', 'rpc.activities.write']}>
 			<Activity />
 		</DiscordContextProvider>
 	)
